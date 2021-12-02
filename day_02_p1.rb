39c158b3c35cf2bc79a086eddff707e5894f9d89
Andrew Katz <andrew.katz@hey.com>
2021-12-02 09:55:12 -0800

    Add day 1 and 2

---
+++ day_02_p1.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+horizontal_position = 0
+depth = 0
+
+File.readlines('day_02_input_2.txt').each do |line|
+  command, units = line.chomp.split(' ')
+  units = units.to_i
+
+  case command
+  when 'forward'
+    horizontal_position += units
+  when 'down'
+    depth += units
+  when 'up'
+    depth -= units
+  end
+end
+
+puts "Final position is #{horizontal_position} * #{depth} = #{horizontal_position * depth}"
---
+++ day_02_p2.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+horizontal_position = 0
+depth = 0
+aim = 0
+
+File.readlines('day_02_input_2.txt').each do |line|
+  command, units = line.chomp.split(' ')
+  units = units.to_i
+
+  case command
+  when 'forward'
+    horizontal_position += units
+    depth += units * aim
+  when 'down'
+    aim += units
+  when 'up'
+    aim -= units
+  end
+end
+
+puts "Final position is #{horizontal_position} * #{depth} = #{horizontal_position * depth}"
