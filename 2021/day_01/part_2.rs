1611f7e8edba23e8ed7ea1a75bba25f15f921ec5
Andrew Katz <andrew.katz@hey.com>
2021-12-16 14:11:03 -1000

    Add Rust solutions for day 1

---
+++ 2021/day_01/part_2.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+// Input 1: 5
+// Input 2: 1633
+
+fn main() {
+    let mut report = Vec::new();
+
+    let contents = fs::read_to_string("input_2.txt")
+        .expect("Something went wrong reading the file");
+
+    let mut index = 0;
+    let mut increased_count = 0;
+
+    for line in contents.trim().split("\n") {
+        let value: i32 = line.parse().expect("Expected a number for line");
+
+        report.push(value);
+
+        if index > 0 && value > report[index - 1] {
+            increased_count += 1;
+        }
+
+        index += 1;
+    }
+
+    println!("Increased {} times", increased_count);
+}
