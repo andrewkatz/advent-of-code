b2c740055e3655dbe38423813ba98f5f833831bc
Andrew Katz <andrew.katz@hey.com>
2021-12-16 14:41:24 -1000

    Add day 13

---
+++ 2021/day_13/part_2.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require_relative '../advent'
+
+dots = []
+folds = []
+
+input('day_13').each do |line|
+  case line
+  when /^\d+,\d+/
+    match_data = line.match(/^(\d+),(\d+)/)
+    x = match_data[1].to_i
+    y = match_data[2].to_i
+    dots << [x, y]
+  when /fold along (x|y)=(\d+)/
+    match_data = line.match(/fold along (x|y)=(\d+)/)
+    axis = match_data[1]
+    value = match_data[2].to_i
+    folds << [axis, value]
+  end
+end
+
+folds.each do |axis, value|
+  dots.each_with_index do |dot, index|
+    x, y = dot
+
+    if axis == 'x'
+      if x == value
+        dots[index] = nil
+      elsif x > value
+        dots[index] = [value - (x - value), y]
+      end
+    elsif y == value
+      dots[index] = nil
+    elsif y > value
+      dots[index] = [x, value - (y - value)]
+    end
+  end
+
+  dots.compact!
+  dots.uniq!
+end
+
+width = dots.map(&:first).max + 1
+height = dots.map(&:last).max + 1
+
+dot_map = {}
+dots.each do |x, y|
+  dot_map[[x, y]] = true
+end
+
+height.times do |y|
+  width.times do |x|
+    if dot_map.key?([x, y])
+      print '#'
+    else
+      print ' '
+    end
+  end
+
+  puts
+end
+
+# Input 1: O
+# Input 2: PGHRKLKL
